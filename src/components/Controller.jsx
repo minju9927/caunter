c2a8d5aa26f650857a9a289de96449bdc19df4e7
minju9927 <pink5214@naver.com>
2025-03-27 11:18:42 +0900

    Add files via upload

---
+++ src/components/Controller.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Controller = ({ buttonValues ,onClickButton }) => {
+    return (
+        <div>
+            {buttonValues.map((value, index) => (
+                <button
+                key={index}
+                onClick={()=>{onClickButton(value)}}
+                >
+                    {value >0 ? `+${value}`:value}
+                </button>
+
+            ))}
+
+        </div>
+    )
+}
+
+export default Controller
